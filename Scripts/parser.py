cb02345ae69aed6d2ff1c9b34e73fc0f45f2679b
Sasa <xxssfamily61@gmail.com>
2016-01-27 11:07:17 +0100

    Update parser.py
    change the way using files.
    and fixed some bugs.

--- Scripts/parser.py
+++ Scripts/parser.py
@@ -1,49 +1,33 @@
-
 # parse to read counters and output what you read into StatOut.csv
 def parseFile():
 
     #you can decide whatever events that is a subset of what you get from bash script you need here.!!!
-    # MUST be the same order , otherwise not enough outputs
+    # MUST be the same order , otherwise not enough outputs :( coupled with RunScript.sh :(
     searchEvents = ["instructions","LLC-loads","LLC-load-misses","LLC-stores","LLC-store-misses","LLC-prefetches","LLC-prefetch-misses","cpu-cycles","page-faults"]
-    #searchEvents = ["context-switches","cpu-migrations","page-faults","instructions"]
 
-    #create StatOut.csv to hold what you will read
-    fw = open("StatOut.csv",'w')
-    # note that: the events'order should be same with CtOutput.txt, because it's searched line by line.
-    fw.write("Workloads")
-    fw.close()
+    # StatOut.csv to hold what you will read
+    with open("StatOut.csv",'w') as fw, open("CtOutput.txt",'r') as fr:
+        fw.write("Workloads," + ",".join(searchEvents) + "\n")
 
-    fw = open("StatOut.csv",'a')
-    for event in searchEvents:
-        fw.write(","),
-        fw.write(event),
+        #read file CtOutput.txt
+        lines = fr.readlines()
 
-    #read file CtOutput.txt
-    fr = open("CtOutput.txt",'r')
-    lines = fr.readlines()
-    fr.close()
+        #gather counters
+        for line in lines:
+            searchWord = "Performance counter stats for "
+            if line.find(searchWord) != -1:
+                app=line[line.index(searchWord)+len(searchWord):len(line)]
+                if app.index("benchspec/CPU2006") != -1:
+                    app=app.split("/")[7]
+                fw.write(app)
 
-    #gather counters
-
-    fw = open("StatOut.csv",'a')
-    fw.write("\n")
-
-    for line in lines:
-        line = line.strip() # get rid of double \n
-        searchWord = "Performance counter stats for "
-        if line.find(searchWord) != -1:
-            fw.write(line[line.index(searchWord)+len(searchWord):len(line)])
-
-        # here are all the counters I need.
-
-        for event in searchEvents:
-            if line.find(event) != -1:
-                pageFaults = line[0:line.find(event)-1]
-                fw.write(",")
-                fw.write(pageFaults)
-                if event is searchEvents[len(searchEvents)-1]:
-                    fw.write("\n")
-
-    fw.close()
+            # here are all the counters I need.
+            for event in searchEvents:
+                if line.find(event) != -1:
+                    counterValue = line[0:line.find(event)-1].replace(" ","")
+                    fw.write(",")
+                    fw.write(counterValue)
+                    if event is searchEvents[len(searchEvents)-1]:
+                        fw.write("\n")
 
 parseFile()
